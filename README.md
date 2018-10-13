e99a5b36a746bd1fc030c37a3326ef47dfddd3f8
Steven Gajek <sgajek@hotmail.com>
2018-10-13 13:05:18 +0100

    Final push

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ps-abuseipreporter
+This repo/script is for reporting IPs from a CSV input file to https://www.abuseipdb.com
+
+## Setup
+* git clone https://github.com/thenecroscope/ps-abuseipreporter.git  
+* git update-index --assume-unchanged configs/config.csv
+  
+Enter your IPs in the ips.csv file and enter your API key from ABUSEIPDB into the config.csv.  
+Note: The API will error if you try to report the same IP details again.
+
+
+### View your requested IP's that have been logged
+https://www.abuseipdb.com/user/XXXXX
+
+
+## List of Categories
+https://www.abuseipdb.com/categories
--- ps-abuseiprepoter.ps1
+++ ps-abuseiprepoter.ps1
@@ -1,1 +1,29 @@
-Write-Host "Hello"
+Clear-Host
+#Getting worklocation and change replace path seperates to help if running on Linux
+$workingDirTemp = $PSCommandPath | Split-Path -Parent
+$workingDir = $workingDirTemp -replace ("\\", "/") #for linux
+
+$apiKey = Get-Content $($workingDir + "/configs/" + "config.csv")
+$ipListToBan = Import-Csv $($workingDir + "/source/" + "ips.csv")
+
+foreach ($ip in $ipListToBan) {
+    try {[ipaddress]$ip.IPTOBLOCK | Out-Null} 
+    catch {
+        Write-Host "----" $($ip.IPTOBLOCK) "is not valid" -ForegroundColor Red
+        Continue
+    } # Valiate if its a good ip, if not, just ignore the value
+    Write-Host "Requesting IP To Be Blocked..."$($ip.IPTOBLOCK)
+    try {
+        $fullResponse = Invoke-WebRequest "https://www.abuseipdb.com/report/json?key=$apiKey&category=$($ip.CATEGORIES)&comment=$($ip.COMMENT)&ip=$($ip.IPTOBLOCK)"     
+        if ($fullResponse.statuscode -eq 200 ) {
+            Write-Host "-" $($ip.IPTOBLOCK)"- Added Successfully" -ForegroundColor Green
+        }
+        else {
+            Write-Host "---" $($ip.IPTOBLOCK)"- Failed To Add" -ForegroundColor Green            
+        }
+    }
+    catch {
+        Write-Host "An Error Has Occured for IP: $($ip.IPTOBLOCK)" -ForegroundColor Red
+    }
+    Write-Host ""
+}
